adaa90e93fb50de6717caab2f91939db2fde96c0
Tero Tikkanen <tero.tikkanen@gmail.com>
2016-09-28 11:09:52 +0300

    Automated commit at 2016-09-28 08:09:52 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -12599,12 +12599,12 @@ $(function() {
    $('#js-theme-kanslia').click(function (){
       $(this).toggleClass('active');
       $('#js-theme-helfi').toggleClass('active');
-      $('link[href="/stylesheets/helfi/theme.css"]').attr('href','/stylesheets/kanslia/theme.css');
+      $('#theme-css').attr('href','stylesheets/kanslia/theme.css');
    });
    $('#js-theme-helfi').click(function (){
       $(this).toggleClass('active');
       $('#js-theme-kanslia').toggleClass('active');
-      $('link[href="/stylesheets/kanslia/theme.css"]').attr('href','/stylesheets/helfi/theme.css');
+      $('#theme-css').attr('href','stylesheets/helfi/theme.css');
    });
 
 });
