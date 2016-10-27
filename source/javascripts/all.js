aec4fc0a3110b74db0e94381fe298ceeff0a7b4a
Tero Tikkanen <tero.tikkanen@gmail.com>
2016-10-27 17:29:14 +0300

    Add sidebar collapse

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -14,4 +14,12 @@ $(function() {
       $('#theme-css').attr('href','stylesheets/helfi/theme.css');
    });
 
+   $('#js-navbar-toggler').click(function (){
+      $(this).toggleClass('is-closed');
+      $('#js-navbar-toggler .glyphicon').toggleClass('glyphicon-remove');
+      $('#js-navbar-toggler .glyphicon').toggleClass('glyphicon-menu-hamburger');
+      $('#js-side-nav').toggleClass('is-hidden');
+      $('#js-site-content').toggleClass('is-full');
+   });
+
 });
